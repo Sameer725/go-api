a1c9e04c1c94dd7e6c8132e2d8b8b8c9c11f46eb
khanal.sameer <57002785+Sameer725@users.noreply.github.com>
2024-01-10 07:16:23 +0545

    feat: json parsing, add validation and errors

--- cmd/api/movies.go
+++ cmd/api/movies.go
@@ -6,10 +6,39 @@ import (
 	"time"
 
 	"movies.samkha.net/internal/data"
+	"movies.samkha.net/internal/validator"
 )
 
 func (app *application) createMovieHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "create new movie")
+	var input struct {
+		Title   string       `json:"title"`
+		Year    int32        `json:"year"`
+		Runtime data.Runtime `json:"runtime"`
+		Genres  []string     `json:"genres"`
+	}
+
+	err := app.readJSON(w, r, &input)
+
+	if err != nil {
+		app.badRequestResponse(w, r, err)
+		return
+	}
+
+	movie := &data.Movie{
+		Title:   input.Title,
+		Year:    input.Year,
+		Runtime: input.Runtime,
+		Genres:  input.Genres,
+	}
+
+	v := validator.New()
+
+	if data.ValidateMovie(v, movie); !v.Valid() {
+		app.failedValidationResponse(w, r, v.Errors)
+		return
+	}
+
+	fmt.Fprintf(w, "%+v", input)
 }
 
 func (app *application) showMovieHandler(w http.ResponseWriter, r *http.Request) {
--- internal/data/runtime.go
+++ internal/data/runtime.go
@@ -1,12 +1,16 @@
 package data
 
 import (
+	"errors"
 	"fmt"
 	"strconv"
+	"strings"
 )
 
 //can customize the json behavior with Marshaller interface, the marshal looks if the struct satisfies the marshaler interface, if it supports it
 // it uses the MarshalJSON method of the interface to convert the struct to json if not fallback to it's own implementation
+
+var ErrInvalidRuntimeFormat = errors.New("invalid runtime format")
 
 type Runtime int32
 
@@ -15,4 +19,28 @@ func (r Runtime) MarshalJSON() ([]byte, error) {
 	quotedJSONValue := strconv.Quote(jsonValue)
 
 	return []byte(quotedJSONValue), nil
+}
+
+func (r *Runtime) UnmarshalJSON(jsonValue []byte) error {
+	unquotedJSONValue, err := strconv.Unquote(string(jsonValue))
+
+	if err != nil {
+		return ErrInvalidRuntimeFormat
+	}
+
+	parts := strings.Split(unquotedJSONValue, " ")
+
+	if len(parts) != 2 || parts[1] != "mins" {
+		return ErrInvalidRuntimeFormat
+	}
+
+	i, err := strconv.ParseInt(parts[0], 10, 32)
+
+	if err != nil {
+		return ErrInvalidRuntimeFormat
+	}
+
+	*r = Runtime(i)
+
+	return nil
 }
