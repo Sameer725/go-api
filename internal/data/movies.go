8ce038474e23aaf5625d62567857a70779b2fc15
khanal.sameer <57002785+Sameer725@users.noreply.github.com>
2024-01-20 08:18:55 +0545

    feat: partial updating and optimistic lock for data race

--- internal/data/movies.go
+++ internal/data/movies.go
@@ -69,10 +69,21 @@ func (model MovieModel) Get(id int64) (*Movie, error) {
 }
 
 func (model MovieModel) Update(movie *Movie) error {
-	query := `UPDATE movies SET title=$1,year=$2,runtime=$3,genres=$4,version=version+1 WHERE id=$5 RETURNING version`
-	args := []any{&movie.Title, &movie.Year, &movie.Runtime, pq.Array(&movie.Genres), &movie.ID}
+	query := `UPDATE movies SET title=$1,year=$2,runtime=$3,genres=$4,version=version+1 WHERE id=$5 AND version=$6 RETURNING version`
+	args := []any{&movie.Title, &movie.Year, &movie.Runtime, pq.Array(&movie.Genres), &movie.ID, &movie.Version}
 
-	return model.DB.QueryRow(query, args...).Scan(&movie.Version)
+	err := model.DB.QueryRow(query, args...).Scan(&movie.Version)
+
+	if err != nil {
+		switch {
+		case errors.Is(err, sql.ErrNoRows):
+			return ErrEditConflict
+		default:
+			return err
+		}
+	}
+
+	return nil
 }
 
 func (model MovieModel) Delete(id int64) error {
