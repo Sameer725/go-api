1d5da73ef3a43e16b4d1cd2665691200f3e5dda0
khanal.sameer <57002785+Sameer725@users.noreply.github.com>
2024-01-21 21:49:08 +0545

    feat: add ratelimiting ip specific with cleanup for old client entry

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -25,6 +25,12 @@ type config struct {
 		maxIdleConns int
 		maxIdleTime  time.Duration
 	}
+
+	limiter struct {
+		rps     float64
+		burst   int
+		enabled bool
+	}
 }
 
 type application struct {
@@ -72,6 +78,10 @@ func main() {
 	flag.IntVar(&cfg.db.maxOpenConns, "db-max-open-conns", 25, "PostgreSQL max open connections.")
 	flag.IntVar(&cfg.db.maxIdleConns, "db-max-idle-conns", 25, "PostgreSQL max idle connections.")
 	flag.DurationVar(&cfg.db.maxIdleTime, "db-max-idle-time", 15*time.Minute, "PostgreSQL max connection idle time")
+
+	flag.Float64Var(&cfg.limiter.rps, "limiter-rps", 2, "Rate limiter max reqs/second")
+	flag.IntVar(&cfg.limiter.burst, "limiter-burst", 4, "Rate limiter max burst")
+	flag.BoolVar(&cfg.limiter.enabled, "limiter-enabled", true, "Rate limiter enabled")
 
 	flag.Parse()
 
