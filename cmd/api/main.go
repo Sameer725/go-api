4ae72aafd68ed7714c40b7b57cd2de5045adc4b1
khanal.sameer <57002785+Sameer725@users.noreply.github.com>
2024-01-28 08:33:28 +0545

    feat: CORS

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -6,6 +6,7 @@ import (
 	"flag"
 	"log/slog"
 	"os"
+	"strings"
 	"sync"
 	"time"
 
@@ -39,6 +40,10 @@ type config struct {
 		username string
 		password string
 		sender   string
+	}
+
+	cors struct {
+		trustedOrigins []string
 	}
 }
 
@@ -99,6 +104,12 @@ func main() {
 	flag.StringVar(&cfg.smtp.username, "smtp-username", "28d8f40e24f563", "SMTP username")
 	flag.StringVar(&cfg.smtp.password, "smtp-password", "038301c78c77f0", "SMTP password")
 	flag.StringVar(&cfg.smtp.sender, "smtp-sender", "Greenlight<no-reply@movies.samkha.net>", "SMTP sender")
+
+	//string.fields splits the flag value into slice based on whitespace
+	flag.Func("cors-trusted-origins", "Trusted CORS origin space separated", func(val string) error {
+		cfg.cors.trustedOrigins = strings.Fields(val)
+		return nil
+	})
 
 	flag.Parse()
 
